82e69b9f28fc3478167ac7487ea4a875423d95de
Philip Steinebrunner <p.steinebrunner@ibitech.com>
2015-12-28 15:10:29 +0100

    app files dont match test files anymore

--- karma.test.shim.js
+++ karma.test.shim.js
@@ -1,13 +1,11 @@
 // Tun on full stack traces in errors to help debugging
 Error.stackTraceLimit=Infinity;
 
-
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
 
 // // Cancel Karma's synchronous start,
 // // we will call `__karma__.start()` later, once all the specs are loaded.
 __karma__.loaded = function() {};
-
 
 System.config({
   packages: {
@@ -20,7 +18,7 @@ System.config({
               // creates local module name mapping to global path with karma's fingerprint in path, e.g.:
               // './hero.service': '/base/src/app/hero.service.js?f4523daf879cfb7310ef6242682ccf10b2041b3e'
               var moduleName = appPath.replace(/^\/base\/src\//, './').replace(/\.js$/, '');
-              pathsMapping[moduleName] = appPath + '?' + window.__karma__.files[appPath]
+              pathsMapping[moduleName] = appPath + '?' + window.__karma__.files[appPath];
               return pathsMapping;
             }, {})
 
@@ -46,18 +44,15 @@ System.import('angular2/src/platform/browser/browser_adapter').then(function(bro
   __karma__.error(error.stack || error);
 });
 
-
 function filePath2moduleName(filePath) {
   return filePath.
            replace(/^\//, '').              // remove / prefix
            replace(/\.\w+$/, '');           // remove suffix
 }
 
-
 function onlyAppFiles(filePath) {
-  return /^\/base\/src\/.*\.js$/.test(filePath)
+  return /^\/base\/src\/\w+\.js$/.test(filePath)
 }
-
 
 function onlySpecFiles(path) {
   return /.spec\.js$/.test(path);
